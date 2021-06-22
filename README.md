cf9021ee11c020cab3cc792be1e77b3b9b4c0fbf
Thang Tran <drakiez92@gmail.com>
2021-06-22 14:25:21 +0700

    fix & test api

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Object Detection and Simulation
 
 ## Description
+
 Final project of Introduction to Computer Vision and Computer Graphics.
 
 Web application to detect objects and simulate those objects model.
 
 ## Usage
+
 ```shell
 docker pull drakiez92/sannin:latest
 docker run -it --rm -p=80:80 --name=sannin drakiez92/sannin flask run -p 80 -h 0.0.0.0
--- app/static/api.js
+++ app/static/api.js
@@ -4,12 +4,11 @@ return data will be executed in callback if success
 handle error with err callback
 */
 function upload(file, callback, err) {
-    fetch('http://127.0.0.1:5000/api/image_upload', {
+    const formData = new FormData();
+    formData.append('imagefile', file);
+    fetch('/api/image_upload', {
       method: 'POST',
-      headers: {
-        "Content-Type": "You will perhaps need to define a content-type here"
-      },
-      body: file
+      body: formData
     }).then(
       response => response.json()
     ).then(
