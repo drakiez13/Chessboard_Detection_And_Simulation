a64e5068a07d54d0f6f072744f1f97f7fe8fcf3e
Minh Tran <19521855@gm.uit.edu.vn>
2021-06-23 23:16:35 +0700

    xoay duoc

--- app/static/js/renderer.js
+++ app/static/js/renderer.js
@@ -1,6 +1,7 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/build/three.module.js';
 import {OBJLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r127/examples/jsm/loaders/OBJLoader.js';
 
+
 function render(place, name) {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
@@ -14,21 +15,26 @@ function render(place, name) {
     // MODEL
 
     var loader = new OBJLoader();
+    var model = null;
 
     loader.load('/models/'+name+'/'+name+'.obj', function(object)
-        { 
-            scene.add(object);
-        });
+    { 
+        model=object;
+        scene.add(model);
+    });
     
 
     // LIGHT
+    
     const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1,1,1);
     scene.add(light);
 
-    
+    //const controls = new THREE.OrbitControls(camera, renderer.domElement);
     function animate() {
         requestAnimationFrame(animate);
-        
+        if (model) {
+            model.rotation.y -= 0.01;}
         renderer.render(scene, camera);
     }
     animate();
