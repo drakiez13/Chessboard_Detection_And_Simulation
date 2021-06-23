16411a7d99a5ead0123e11eb0989b79f310d3aa4
Thang Tran <drakiez92@gmail.com>
2021-06-23 20:21:09 +0700

    add next/previous model switch

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,18 +1,44 @@
 import upload from './api.js'
 import render from './renderer.js'
 
-function createChild(name)
-{
-    let child = document.createElement('h1');
-    child.innerHTML = name;
-    return child;
+let data = [];
+let pos = 0;
+
+let incInfo = e => {
+    pos = pos + 1;
+    if (pos > data.length)
+        pos = 1;
+    renderObjects(pos);
 }
 
-function renderObjects(data)
+let decInfo = e => {
+    pos = pos -1;
+    if (pos < 1)
+        pos = data.length;
+    renderObjects(pos);
+}
+
+let nextbtn = document.getElementById('next-btn');
+nextbtn.onclick = incInfo;
+let prevbtn = document.getElementById('prev-btn');
+prevbtn.onclick = decInfo;
+
+function renderObjects(pos)
 {
     let renderDOM = document.getElementById('content');
     renderDOM.innerHTML = '';
-    render(renderDOM, data[0].name, 400, 400)
+
+    let width = renderDOM.offsetWidth;
+    if (width > 700)
+        width = 700;
+
+    let infoElement = document.createElement('h5');
+    renderDOM.appendChild(infoElement);
+
+    let numOfObjects = data.length;
+    infoElement.innerHTML = pos + "/" + numOfObjects;
+
+    render(renderDOM, data[pos - 1].name, width, width);
 }
 
 function displayImg(event) {
@@ -36,14 +62,16 @@ function imgUploadHandler() {
         console.log('[Error] ' + err);
     }
     
-    function successHandler(data) {
-        if (data.length === 0)
+    function successHandler(res) {
+        if (res.length === 0)
         {
             alert("Cannot detect any object");
         }
         else
         {
-            renderObjects(data);
+            data = res;
+            pos = 1;
+            renderObjects(pos);
         }
     }
 
