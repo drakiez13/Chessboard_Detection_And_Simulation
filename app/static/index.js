5767a111bf713d5df2f69c3996948f2b521d0945
Thang Tran <drakiez92@gmail.com>
2021-06-23 13:55:00 +0700

    sample for index

--- app/static/index.js
+++ app/static/index.js
@@ -1,2 +1,35 @@
-document.getElementById("demo").innerHTML = "App works!";
+function createChild(name)
+{
+    let child = document.createElement('h1');
+    child.innerHTML = name;
+    return child;
 
+}
+
+function renderObjects(data)
+{
+    let renderDOM = document.getElementById('render');
+    data.forEach(element => {
+        renderDOM.appendChild(createChild(element.name));
+    });
+}
+
+function imgUploadHandler() {
+    function errHandler(err) {
+        console.log('[Error] ' + err);
+    }
+    
+    function successHandler(data) {
+        if (data.length === 0)
+        {
+            alert("Cannot detect any object");
+        }
+        else
+        {
+            renderObjects(data);
+        }
+    }
+
+    let imageFile = document.getElementById('myFile').files[0];
+    upload(imageFile, successHandler, errHandler);
+}
