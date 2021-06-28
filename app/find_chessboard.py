3b112a756a0b9d8c8bbb62c5ca91ae9fe8d3a81a
Thang Tran <drakiez92@gmail.com>
2021-06-28 16:31:47 +0700

    add test api endpoint

--- app/find_chessboard.py
+++ app/find_chessboard.py
@@ -4,4 +4,4 @@ def convert_to_position(topleft, topright, botleft, botright):
     pass
 
 def find_chessboard(img):
-    return dict({'isDetected': 'fail', 'positions': None})
+    return dict({'isDetected': 'ok', 'positions': [ {'name': 'black-king', 'x':1, 'y':1}]})
